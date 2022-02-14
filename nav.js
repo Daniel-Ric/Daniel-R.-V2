f93786ddee79c4a18a0d0a174b850b40e36a1860
Daniel-Designer <daanny.rickers@gmail.com>
2022-02-14 10:09:24 +0100

    Some slide changes

--- nav.js
+++ nav.js
@@ -4,17 +4,17 @@
  *  File : nav.js
  *******************************************/
 
- const hamburger = document.querySelector(".hamburger");
- const navMenu = document.querySelector(".nav-menu");
- 
- hamburger.addEventListener("click", mobileMenu);
- 
- function mobileMenu() {
-     hamburger.classList.toggle("active");
-     navMenu.classList.toggle("active");
- }
- 
- const navLink = document.querySelectorAll(".nav-link");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
 
 navLink.forEach(n => n.addEventListener("click", closeMenu));
 

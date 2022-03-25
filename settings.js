180c774c4e56442e60bc3b2f53fcce6a1de0bece
Daniel <daanny.rickers@gmail.com>
2022-03-26 00:04:16 +0100

    Tested the Typed.js libary

---
+++ settings.js
@@ -0,0 +1,12 @@
+/******************************************
+ *  Author : Author   
+ *  Created On : Fri Mar 25 2022
+ *  File : settings.js
+ *******************************************/
+
+ var typed = new Typed('.keywords', {
+    strings: ['Photographer','Front- Backend Developer', ' Digital Artist'],
+    loop: true,
+    typeSpeed: 180,
+    backSpeed: 180
+  });
